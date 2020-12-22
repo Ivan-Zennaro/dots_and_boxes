6070aca032f2e5f5a94ce4c8faeb85bedb66173c
Dario Crosera <dario.cro@hotmail.it>
2020-12-22 18:03:48 +0100

    class Box skeleton created

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -1,2 +1,20 @@
 public class Box {
+
+    private boolean lineUp;
+    private boolean lineDown;
+    private boolean lineLeft;
+    private boolean lineRight;
+
+
+    /*
+        Dont know if could be useful, depends on the implementation.
+        [Dario]
+     */
+    private Player closedByPlayer;
+
+    public Box() {
+    }
+
+
+
 }
