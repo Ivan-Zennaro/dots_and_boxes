41f2e1c6d4342718deb4a7e734b98b9683ed37c8
ivanzen97 <ivanzen@live.it>
2020-12-22 15:59:45 +0100

    added color to player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -2,9 +2,11 @@ public class Player {
 
     private char id;
     private int points = 0;
+    private Color color;
 
-    public Player(char id){
+    public Player(char id, Color color){
         this.id = id;
+        this.color = color;
     }
 
 }
