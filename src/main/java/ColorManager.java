5738199f8f28dd42709eb59b3f0488143de72b58
ivanzen97 <ivanzen@live.it>
2020-12-22 16:22:31 +0100

    adapt method getColoredString for Travis

--- src/main/java/ColorManager.java
+++ src/main/java/ColorManager.java
@@ -1,7 +1,26 @@
 public class ColorManager {
 
     public static String getColoredString (String s, Color color){
-        return (
+
+        String s2 = s + "\u001B[0m";
+
+        switch (color) {
+            case BLU: s =  "\u001B[34m";
+                        break;
+            case RED: s =  "\u001B[31m";
+                break;
+            case PURPLE: s =  "\u001B[35m";
+                break;
+            case GREEN: s =  "\u001B[32m";
+                break;
+            default: s =  "\u001B[0m";
+        }
+
+        return s + s2;
+
+
+
+        /*return (
             switch (color){
                 case BLU -> "\u001B[34m";
                 case RED -> "\u001B[31m";
@@ -10,5 +29,7 @@ public class ColorManager {
                 default -> "\u001B[0m";
             }
         ) + s + "\u001B[0m" ;
+
+         */
     }
 }
