815781206e69f74b2c08042a20e737b2d1dc8f6e
ivanzen97 <ivanzen@live.it>
2020-12-22 17:38:55 +0100

    refactor coloredString method

--- src/main/java/ColorManager.java
+++ src/main/java/ColorManager.java
@@ -1,35 +1,25 @@
 public class ColorManager {
 
-    public static String getColoredString (String s, Color color){
+    public static String getColoredString(String string, Color color) {
 
-        String s2 = s + "\u001B[0m";
+        String colorPattern;
 
         switch (color) {
-            case BLU: s =  "\u001B[34m";
-                        break;
-            case RED: s =  "\u001B[31m";
+            case BLU:
+                colorPattern = "\u001B[34m";
                 break;
-            case PURPLE: s =  "\u001B[35m";
+            case RED:
+                colorPattern = "\u001B[31m";
                 break;
-            case GREEN: s =  "\u001B[32m";
+            case PURPLE:
+                colorPattern = "\u001B[35m";
                 break;
-            default: s =  "\u001B[0m";
+            case GREEN:
+                colorPattern = "\u001B[32m";
+                break;
+            default:
+                colorPattern = "\u001B[0m";
         }
-
-        return s + s2;
-
-
-
-        /*return (
-            switch (color){
-                case BLU -> "\u001B[34m";
-                case RED -> "\u001B[31m";
-                case PURPLE -> "\u001B[35m";
-                case GREEN -> "\u001B[32m";
-                default -> "\u001B[0m";
-            }
-        ) + s + "\u001B[0m" ;
-
-         */
+        return colorPattern + string + "\u001B[0m";
     }
 }
