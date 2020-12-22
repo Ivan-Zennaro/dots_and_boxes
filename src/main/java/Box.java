ba8818e7112c74cd882fbfad290afe41c81580a5
Dario Crosera <dario.cro@hotmail.it>
2020-12-22 18:41:47 +0100

    class Box updated with getters and draw-liners methods

--- src/main/java/Box.java
+++ src/main/java/Box.java
@@ -8,13 +8,54 @@ public class Box {
 
     /*
         Dont know if could be useful, depends on the implementation.
+        If class Game keep track of which player close a box then this is useless.
         [Dario]
      */
     private Player closedByPlayer;
 
     public Box() {
+        this.lineUp = false;
+        this.lineDown = false;
+        this.lineLeft = false;
+        this.lineRight = false;
     }
 
+    public Box(boolean lineUp, boolean lineLeft, boolean lineDown, boolean lineRight) {
+        this.lineUp = lineUp;
+        this.lineDown = lineDown;
+        this.lineLeft = lineLeft;
+        this.lineRight = lineRight;
+    }
+
+    public boolean hasLineUp() {
+        return lineUp;
+    }
+
+    public boolean hasLineDown() {
+        return lineDown;
+    }
+
+    public boolean hasLineLeft() {
+        return lineLeft;
+    }
+
+    public boolean hasLineRight() {
+        return lineRight;
+    }
+
+
+    public void drawLineUp(){
+        lineUp = true;
+    }
+    public void drawLineDown(){
+        lineDown = true;
+    }
+    public void drawLineLeft(){
+        lineLeft = true;
+    }
+    public void drawLineRight(){
+        lineRight = true;
+    }
 
 
 }
