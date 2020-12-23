b009677c5c9db1e7b71c1fdacd93ba029b4b8983
Domenis Nicola S262688 <nicoladomenis23@hotmail.it>
2020-12-23 22:05:13 +0100

    Modified some classes to update neighbour box

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,10 +1,13 @@
 public class Board {
 
     private Box[][] board;
-
+    int boxRows;
+    int boxColumns;
 
     public Board(int numberOfBoxesInARow, int numberOfBoxesInAColumn) {
         board = new Box[numberOfBoxesInARow][numberOfBoxesInAColumn];
+        this.boxRows = numberOfBoxesInARow;
+        this.boxColumns = numberOfBoxesInAColumn;
         for (int i = 0; i < numberOfBoxesInARow; i++) {
             for (int j = 0; j < numberOfBoxesInAColumn; j++) {
                 board[i][j] = new Box();
@@ -21,7 +24,31 @@ public class Board {
         //devo disegnare anche la linea nella box adiacente
         Box choosenBox = board[move.getX()][move.getY()];
         choosenBox.drawLine(move.getSide());
-    }
+        Box otherBox = choosenBox;
 
+
+        boolean flag = false;
+            if(move.getSide() == Side.UP || move.getSide() == Side.DOWN)
+                if(isMoveInBoardX(move.getX()+move.getSide().new_coord())){
+                     otherBox = board[move.getX()+move.getSide().new_coord()][move.getY()];
+                     flag = true;
+              }
+            if(move.getSide() == Side.LEFT || move.getSide() == Side.RIGHT)
+                 if(isMoveInBoardY(move.getY()+move.getSide().new_coord())){
+                     otherBox = board[move.getX()][move.getY()+move.getSide().new_coord()];
+                     flag = true;
+                 }
+                 if (flag)
+                        otherBox.drawLine(move.getSide().invert());
+    }
+    public boolean isMoveInBoardRange(Move move){
+        return move.getX() < boxRows && move.getY() < boxColumns && move.getX() > 0 && move.getY() >0;
+    }
+    public boolean isMoveInBoardX(int coord){
+        return coord < boxRows && coord > 0 ;
+    }
+    public boolean isMoveInBoardY(int coord){
+        return  coord < boxColumns && coord > 0 ;
+    }
 
 }
--- src/main/java/Side.java
+++ src/main/java/Side.java
@@ -6,4 +6,19 @@ public enum Side {
     RIGHT,
     INVALID;
     public char asChar(){return name().charAt(0);}
+    public  Side invert(){
+        if(this == UP) return DOWN;
+        if(this == DOWN) return UP;
+        if(this == LEFT) return RIGHT;
+        if(this == RIGHT) return LEFT;
+        return this;
+    }
+    public int new_coord(){
+        if(this == UP) return -1;
+        if(this == DOWN) return +1;
+        if(this == LEFT) return -1;
+        if(this == RIGHT) return +1;
+        return 0;
+
+    }
 }
