27252403ac9902b1268ead68435c27c03fb3ff25
ivanzen97 <ivanzen@live.it>
2020-12-23 21:34:34 +0100

    delete comments in color manager

--- src/main/java/ColorManager.java
+++ src/main/java/ColorManager.java
@@ -1,7 +1,6 @@
 public class ColorManager {
 
     public static String getColoredString(String string, Color color) {
-
         return (
                 switch (color) {
                     case BLU -> "\u001B[34m";
@@ -11,25 +10,5 @@ public class ColorManager {
                     default -> "\u001B[0m";
                 }
         ) + string + "\u001B[0m";
-
-        /*String colorPattern;
-
-        switch (color) {
-            case BLU:
-                colorPattern = "\u001B[34m";
-                break;
-            case RED:
-                colorPattern = "\u001B[31m";
-                break;
-            case PURPLE:
-                colorPattern = "\u001B[35m";
-                break;
-            case GREEN:
-                colorPattern = "\u001B[32m";
-                break;
-            default:
-                colorPattern = "\u001B[0m";
-        }
-        return colorPattern + string + "\u001B[0m";*/
     }
 }
