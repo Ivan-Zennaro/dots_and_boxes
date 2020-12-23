5b66c2e752241fcafab5cb0d953930d345f7614e
ivanzen97 <ivanzen@live.it>
2020-12-23 21:31:51 +0100

    build.gradle add java version 15

--- src/main/java/ColorManager.java
+++ src/main/java/ColorManager.java
@@ -2,7 +2,17 @@ public class ColorManager {
 
     public static String getColoredString(String string, Color color) {
 
-        String colorPattern;
+        return (
+                switch (color) {
+                    case BLU -> "\u001B[34m";
+                    case RED ->"\u001B[31m";
+                    case PURPLE ->"\u001B[35m";
+                    case GREEN ->"\u001B[32m";
+                    default -> "\u001B[0m";
+                }
+        ) + string + "\u001B[0m";
+
+        /*String colorPattern;
 
         switch (color) {
             case BLU:
@@ -20,6 +30,6 @@ public class ColorManager {
             default:
                 colorPattern = "\u001B[0m";
         }
-        return colorPattern + string + "\u001B[0m";
+        return colorPattern + string + "\u001B[0m";*/
     }
 }
