d3bd418535c94a9e20a99bb879b90993e73fbacd
Luca Crozzoli <crozzoliluca9@gmail.com>
2020-12-23 12:41:49 +0100

    Test boxHasAlreadyLineUP passed

--- src/test/java/TestBoard.java
+++ src/test/java/TestBoard.java
@@ -1,12 +1,15 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestBoard {
 
-
-
-
+    @Test
+    public void checkboxHasAlreadyLineUP(){
+        Board board = new Board(1,1);
+        Move move = new Move(0,0,Side.UP);
+        board.drawLine(move);
+        assertTrue(board.boxHasAlreadyLine(move));
+    }
 
 }
