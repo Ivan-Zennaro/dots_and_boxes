91fbe649d6238fc759e91593d2f822f9fff81179
ivanzen97 <ivanzen@live.it>
2020-12-24 13:48:54 +0100

    added 3x3 empty board test

--- src/test/java/TestGraphic.java
+++ src/test/java/TestGraphic.java
@@ -18,6 +18,20 @@ public class TestGraphic {
         Assertions.assertEquals(boardString_2x2,graphic.getStringBoard());
     }
 
+     @Test
+    public void draw_3x3_empty_board(){
+         Graphic graphic = new Graphic(3,3);
+        String boardString_3x3 =
+                "\n" + " --- --- ---\n" +
+                "|   |   |   |   \n" +
+                " --- --- ---\n" +
+                "|   |   |   |   \n" +
+                " --- --- ---\n" +
+                "|   |   |   |   \n" +
+                " --- --- ---\n";
+         Assertions.assertEquals(boardString_3x3,graphic.getStringBoard());
+    }
+
 
     @Test
     public void update_red_move_in_2x2_board(){
@@ -32,6 +46,7 @@ public class TestGraphic {
 
         Assertions.assertEquals(boardString_2x2,graphic.getStringBoard());
     }
+
 
     @ParameterizedTest
     @CsvSource({"1,UP,1", "2,DOWN,2", "3,RIGHT,4", "5,LEFT,5"})
