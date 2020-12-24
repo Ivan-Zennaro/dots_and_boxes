69da71572274f085e45327afe005df35f777daa3
ivanzen97 <ivanzen@live.it>
2020-12-24 11:57:25 +0100

    grapic constructor complete

--- src/main/java/Graphic.java
+++ src/main/java/Graphic.java
@@ -1,25 +1,39 @@
 public class Graphic {
 
-    private String board [][];
+    private String graphicBoard [][];
 
     public Graphic (int boardRows, int boardCols){
-        board = new String[boardRows * 2 + 1][boardCols + 1];
+        int mappedRows = boardRows * 2 + 1;
+        int mappedCols = boardCols + 1;
+        graphicBoard = new String[mappedRows][mappedCols];
+        for (int i = 0; i < mappedRows; i++){
+            for (int j = 0 ; j < mappedCols ; j++) {
+                if (i % 2 == 0)
+                    graphicBoard[i][j] = " ---";
+                else
+                    graphicBoard[i][j] = "|   ";
+                }
+            }
     }
+
 
     public void updateMove (Move move, Player player){
 
     }
 
     public String getStringBoard() {
-        String boardString_2x2 =
-                " --- ---\n" +
-                        "|   |   |\n" +
-                        " --- ---\n" +
-                        "|   |   |\n" +
-                        " --- ---\n" +
-                        "\n";
 
-        return boardString_2x2;
+        String s = "\n";
+        for (int i = 0; i < graphicBoard.length; i++){
+            for (int j = 0; (i % 2 == 0 &&  j < graphicBoard[0].length -1 ) || ( i % 2 != 0 &&  j < graphicBoard[0].length ); j++){
+                s += graphicBoard[i][j];
+            }
+            s += "\n";
+        }
+
+
+
+        return s;
     }
 
 }
--- src/test/java/TestGraphic.java
+++ src/test/java/TestGraphic.java
@@ -7,12 +7,11 @@ public class TestGraphic {
     public void draw_2x2_empty_board(){
         Graphic graphic = new Graphic(2,2);
         String boardString_2x2 =
+                "\n"+ " --- ---\n" +
+                "|   |   |   \n" +
                 " --- ---\n" +
-                "|   |   |\n" +
-                " --- ---\n" +
-                "|   |   |\n" +
-                " --- ---\n" +
-                "\n";
+                "|   |   |   \n" +
+                " --- ---\n";
         Assertions.assertEquals(boardString_2x2,graphic.getStringBoard());
     }
 }
