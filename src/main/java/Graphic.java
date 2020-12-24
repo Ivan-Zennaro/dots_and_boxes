51c173fb50c06f2b78154d289eb65de5ad54fb4b
ivanzen97 <ivanzen@live.it>
2020-12-24 13:55:04 +0100

    auto formatting of graphic and testGraphic

--- src/main/java/Graphic.java
+++ src/main/java/Graphic.java
@@ -3,44 +3,44 @@ public class Graphic {
     private static final String HORIZZONTAL_LINE = " ---";
     private static final String VERTICAL_LINE = "|   ";
 
-    private String graphicBoard [][];
+    private String graphicBoard[][];
 
-    public Graphic (int boardRows, int boardCols){
+    public Graphic(int boardRows, int boardCols) {
         int mappedRows = boardRows * 2 + 1;
         int mappedCols = boardCols + 1;
         graphicBoard = new String[mappedRows][mappedCols];
-        for (int i = 0; i < mappedRows; i++){
-            for (int j = 0 ; j < mappedCols ; j++) {
+        for (int i = 0; i < mappedRows; i++) {
+            for (int j = 0; j < mappedCols; j++) {
                 if (i % 2 == 0)
                     graphicBoard[i][j] = HORIZZONTAL_LINE;
                 else
                     graphicBoard[i][j] = VERTICAL_LINE;
-                }
             }
+        }
     }
 
 
-    public void updateMove (Move move, Player player){
+    public void updateMove(Move move, Player player) {
 
         Color color = player.getColor();
         int mappedX = mapX(move);
         int mappedY = mapY(move);
         String stringToWrite;
         if (mappedX % 2 == 0)
-            stringToWrite = ColorManager.getColoredString(HORIZZONTAL_LINE,color);
+            stringToWrite = ColorManager.getColoredString(HORIZZONTAL_LINE, color);
         else
-            stringToWrite = ColorManager.getColoredString(VERTICAL_LINE,color);
+            stringToWrite = ColorManager.getColoredString(VERTICAL_LINE, color);
 
         graphicBoard[mappedX][mappedY] = stringToWrite;
     }
 
-    public static int mapY (Move move){
+    public static int mapY(Move move) {
         if (move.getSide() == Side.RIGHT)
             return move.getY() + 1;
         else return move.getY();
     }
 
-    public static int mapX (Move move){
+    public static int mapX(Move move) {
         int tempX = move.getX() * 2 + 1;
         if (move.getSide() == Side.UP)
             return tempX - 1;
@@ -51,8 +51,8 @@ public class Graphic {
 
     public String getStringBoard() {
         String s = "\n";
-        for (int i = 0; i < graphicBoard.length; i++){
-            for (int j = 0; (i % 2 == 0 &&  j < graphicBoard[0].length -1 ) || ( i % 2 != 0 &&  j < graphicBoard[0].length ); j++){
+        for (int i = 0; i < graphicBoard.length; i++) {
+            for (int j = 0; (i % 2 == 0 && j < graphicBoard[0].length - 1) || (i % 2 != 0 && j < graphicBoard[0].length); j++) {
                 s += graphicBoard[i][j];
             }
             s += "\n";
--- src/test/java/TestGraphic.java
+++ src/test/java/TestGraphic.java
@@ -2,81 +2,75 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestGraphic {
 
     @Test
-    public void draw_2x2_empty_board(){
-        Graphic graphic = new Graphic(2,2);
+    public void draw_2x2_empty_board() {
+        Graphic graphic = new Graphic(2, 2);
         String boardString_2x2 =
-                "\n"+ " --- ---\n" +
-                "|   |   |   \n" +
-                " --- ---\n" +
-                "|   |   |   \n" +
-                " --- ---\n";
-        Assertions.assertEquals(boardString_2x2,graphic.getStringBoard());
+                "\n" + " --- ---\n" +
+                        "|   |   |   \n" +
+                        " --- ---\n" +
+                        "|   |   |   \n" +
+                        " --- ---\n";
+        Assertions.assertEquals(boardString_2x2, graphic.getStringBoard());
     }
 
     @Test
-    public void draw_3x3_empty_board(){
-        Graphic graphic = new Graphic(3,3);
+    public void draw_3x3_empty_board() {
+        Graphic graphic = new Graphic(3, 3);
         String boardString_3x3 =
                 "\n" + " --- --- ---\n" +
-                "|   |   |   |   \n" +
-                " --- --- ---\n" +
-                "|   |   |   |   \n" +
-                " --- --- ---\n" +
-                "|   |   |   |   \n" +
-                " --- --- ---\n";
-         Assertions.assertEquals(boardString_3x3,graphic.getStringBoard());
+                        "|   |   |   |   \n" +
+                        " --- --- ---\n" +
+                        "|   |   |   |   \n" +
+                        " --- --- ---\n" +
+                        "|   |   |   |   \n" +
+                        " --- --- ---\n";
+        Assertions.assertEquals(boardString_3x3, graphic.getStringBoard());
     }
 
 
     @Test
-    public void update_blu_move_in_2x2_board(){
-        Graphic graphic = new Graphic(2,2);
-        graphic.updateMove(new Move(0,0,Side.UP),new Player('A',Color.BLU));
+    public void update_blu_move_in_2x2_board() {
+        Graphic graphic = new Graphic(2, 2);
+        graphic.updateMove(new Move(0, 0, Side.UP), new Player('A', Color.BLU));
         String boardString_2x2 =
-                "\n"+ ColorManager.getColoredString(" ---",Color.BLU) +" ---\n" +
+                "\n" + ColorManager.getColoredString(" ---", Color.BLU) + " ---\n" +
                         "|   |   |   \n" +
                         " --- ---\n" +
                         "|   |   |   \n" +
                         " --- ---\n";
 
-        Assertions.assertEquals(boardString_2x2,graphic.getStringBoard());
+        Assertions.assertEquals(boardString_2x2, graphic.getStringBoard());
     }
 
     @Test
-    public void update_blu_move_in_3x3_board(){
-        Graphic graphic = new Graphic(3,3);
-        graphic.updateMove(new Move(2,2,Side.RIGHT),new Player('A',Color.BLU));
+    public void update_blu_move_in_3x3_board() {
+        Graphic graphic = new Graphic(3, 3);
+        graphic.updateMove(new Move(2, 2, Side.RIGHT), new Player('A', Color.BLU));
         String boardString_3x3 =
                 "\n" + " --- --- ---\n" +
                         "|   |   |   |   \n" +
                         " --- --- ---\n" +
                         "|   |   |   |   \n" +
                         " --- --- ---\n" +
-                        "|   |   |   " + ColorManager.getColoredString("|   ",Color.BLU) +"\n" +
+                        "|   |   |   " + ColorManager.getColoredString("|   ", Color.BLU) + "\n" +
                         " --- --- ---\n";
 
-        Assertions.assertEquals(boardString_3x3,graphic.getStringBoard());
+        Assertions.assertEquals(boardString_3x3, graphic.getStringBoard());
     }
-
 
     @ParameterizedTest
     @CsvSource({"1,UP,1", "2,DOWN,2", "3,RIGHT,4", "5,LEFT,5"})
-    public void map_y_value_of_game_board_with_y_value_of_graphic_board(int gameBoardY,Side side, int graphicBoardY){
-        Assertions.assertEquals(graphicBoardY,Graphic.mapY(new Move(0,gameBoardY,side)));
+    public void map_y_value_of_game_board_with_y_value_of_graphic_board(int gameBoardY, Side side, int graphicBoardY) {
+        Assertions.assertEquals(graphicBoardY, Graphic.mapY(new Move(0, gameBoardY, side)));
     }
 
     @ParameterizedTest
     @CsvSource({"0,UP,0", "2,DOWN,6", "1,RIGHT,3", "2,UP,4"})
-    public void map_x_value_of_game_board_with_x_value_of_graphic_board(int gameBoardX,Side side, int graphicBoardX){
-        Assertions.assertEquals(graphicBoardX,Graphic.mapX(new Move(gameBoardX,0,side)));
+    public void map_x_value_of_game_board_with_x_value_of_graphic_board(int gameBoardX, Side side, int graphicBoardX) {
+        Assertions.assertEquals(graphicBoardX, Graphic.mapX(new Move(gameBoardX, 0, side)));
     }
-
-
-
-
 }
