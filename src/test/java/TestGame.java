110f62041f57ab270c7a2579649814b842cf3eb0
ivanzen97 <ivanzen@live.it>
2020-12-26 11:41:05 +0100

    generalized test check_input_grid_dim

--- src/test/java/TestGame.java
+++ src/test/java/TestGame.java
@@ -1,18 +1,19 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
-import java.io.InputStream;
 
 public class TestGame {
 
-    @Test
-    public void check_input_grid_dimensions_2x2(){
-        ByteArrayInputStream in = new ByteArrayInputStream("2".getBytes());
+    @ParameterizedTest
+    @ValueSource(strings = {"2","3","5"}) //Value Source quando ho solo un parametro
+    public void check_input_grid_dimensions(String choice){
+        ByteArrayInputStream in = new ByteArrayInputStream(choice.getBytes());
         System.setIn(in);
         Game game = new Game();
         Board board = game.initializeBoard();
-        Assertions.assertEquals(2, board.getBoardRows());
+        Assertions.assertEquals(Integer.parseInt(choice), board.getBoardRows());
     }
 
 }
