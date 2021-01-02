ceec547d50da8fadfbfdecda59b075b097e7d1d7
JawaCoder <johndoe@example.com>
2021-01-02 18:31:52 +0100

    Add new field outputServer, thus edited constructor, override endGame() and finalGraphics, add printWinner

--- src/main/java/ClientPlayerGame.java
+++ src/main/java/ClientPlayerGame.java
@@ -1,14 +1,20 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
-import java.util.Scanner;
 
 public class ClientPlayerGame extends Game {
     BufferedReader keyboard;
+    BufferedWriter outputServer;
+
+    ClientPlayerGame(BufferedReader keyboard, BufferedWriter outputFromServer) {
+        super();
+        this.keyboard = keyboard;
+        this.outputServer = outputFromServer;
+    }
 
     public void startGameServer(BufferedReader input, BufferedWriter output, String quitCommand) throws IOException {
         boolean completedInitializationBoard;
-        completedInitializationBoard = initializeBoard(input, output, quitCommand);
+        completedInitializationBoard = initializeBoard(output, quitCommand);
 
         if (completedInitializationBoard) {
             while (!isGameFinished()) {
@@ -45,9 +51,7 @@ public class ClientPlayerGame extends Game {
     }
 
 
-    public boolean initializeBoard(BufferedReader inputClient, BufferedWriter outputClient, String quitCmd) throws IOException {
-        keyboard = new BufferedReader(inputClient);
-
+    public boolean initializeBoard(BufferedWriter outputClient, String quitCmd) throws IOException {
         int optionGrid = 1;
         boolean allOkInitialization = true;
         String inputLineClient;
@@ -78,22 +82,63 @@ public class ClientPlayerGame extends Game {
         return allOkInitialization;
     }
 
-    public void startGame() {
-        return;
-    }
-
-    public Board initializeBoard() {
-        Board board1 = new Board(1, 1);
-        return board1;
-    }
-
     public String printScoreBoardOnClient() {
-
         String printableScoreBoard;
         printableScoreBoard = graphic.getStringBoard() + System.lineSeparator();
         printableScoreBoard += "Player " + player1.getId() + " got " + player1.getPoints() + " points" + System.lineSeparator();
         printableScoreBoard += "Player " + player2.getId() + " got " + player2.getPoints() + " points" + System.lineSeparator();
         printableScoreBoard += "Is the turn of Player" + currentPlayer.getId() + System.lineSeparator();
         return printableScoreBoard;
+    }
+
+    @Override
+    public void endGame() {
+        finalGraphics();
+        printWinner();
+    }
+
+    @Override
+    public void finalGraphics() {
+        String finalGraphicForClient;
+        finalGraphicForClient = graphic.getStringBoard() + System.lineSeparator();
+        finalGraphicForClient += "Player " + player1.getId() + " got " + player1.getPoints() + " points" + System.lineSeparator();
+        finalGraphicForClient += "Player " + player2.getId() + " got " + player2.getPoints() + " points" + System.lineSeparator();
+
+        try {
+            outputServer.write(finalGraphicForClient);
+            outputServer.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void printWinner() {
+        try {
+            if (player1.getPoints() > player2.getPoints()) {
+                outputServer.write("Player" + player1.getId() + " WON!");
+            } else {
+                if (player2.getPoints() > player1.getPoints()) {
+                    outputServer.write("Player " + player2.getId() + " WON!");
+                } else {
+                    outputServer.write("TIE!");
+                }
+            }
+            outputServer.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //NON SERVE A NIENTE, SOLO PERCHE GAME E' ABSTRACT...Come ottimizzare???
+    public void startGame() {
+        return;
+    }
+
+    //NON SERVE A NIENTE, SOLO PERCHE GAME E' ABSTRACT...
+    public Board initializeBoard() {
+        Board board1 = new Board(1, 1);
+        return board1;
     }
 }
