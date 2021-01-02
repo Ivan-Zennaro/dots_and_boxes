a886d946e3d56f04908a8f180196fa52155ae0fe
JawaCoder <johndoe@example.com>
2021-01-02 18:02:20 +0100

    Add printScoreBoardOnClient method, changed initializeBoard, corrected flow in startGameServer method

--- src/main/java/ClientPlayerGame.java
+++ src/main/java/ClientPlayerGame.java
@@ -7,48 +7,67 @@ public class ClientPlayerGame extends Game {
     BufferedReader keyboard;
 
     public void startGameServer(BufferedReader input, BufferedWriter output, String quitCommand) throws IOException {
-        initializeBoard(input, output);
+        boolean completedInitializationBoard;
+        completedInitializationBoard = initializeBoard(input, output, quitCommand);
 
-        while (!isGameFinished()) {
-            String command = input.readLine();
+        if (completedInitializationBoard) {
+            while (!isGameFinished()) {
 
-            if (command == null) {
-                System.out.print(String.format("[%1$tY-%1$tm-%1$td %1$tT] ", System.currentTimeMillis()));
-                System.out.println("Client abruptly closed connection ");
-                break;
+                output.write(printScoreBoardOnClient());
+                output.flush();
+                System.out.println("Insert move [x y side:U,D,L,R]?");
+                output.write("Insert move [x y side:U,D,L,R]?" + System.lineSeparator());
+                output.flush();
+
+                String command = input.readLine();
+                if (command == null) {
+                    System.out.print(String.format("[%1$tY-%1$tm-%1$td %1$tT] ", System.currentTimeMillis()));
+                    System.out.println("Client abruptly closed connection ");
+                    break;
+                }
+                command = command.trim();
+                if (command.equals(quitCommand)) {
+                    System.out.print(String.format("[%1$tY-%1$tm-%1$td %1$tT] ", System.currentTimeMillis()));
+                    System.out.println("Client disconnected");
+                    break;
+                }
+
+                turn(command);
+
+                //SERVONO????
+                // output.write(graphic.getStringBoard() + System.lineSeparator());
+                // output.flush();
+
             }
-
-            command = command.trim();
-            if (command.equals(quitCommand)) {
-                System.out.print(String.format("[%1$tY-%1$tm-%1$td %1$tT] ", System.currentTimeMillis()));
-                System.out.println("Client disconnected");
-                break;
-            }
-            printScoreBoard();
-            System.out.println("Insert move [x y side:U,D,L,R]?");
-            output.write("Insert move [x y side:U,D,L,R]?" + System.lineSeparator());
-            output.flush();
-            turn(keyboard.readLine());
-            output.write(graphic.getStringBoard() + System.lineSeparator());
-            output.flush();
+            endGame();
         }
-        endGame();
         keyboard.close();
     }
 
 
-    public Board initializeBoard(BufferedReader inputClient, BufferedWriter outputClient) throws IOException {
+    public boolean initializeBoard(BufferedReader inputClient, BufferedWriter outputClient, String quitCmd) throws IOException {
         keyboard = new BufferedReader(inputClient);
 
         int optionGrid = 1;
+        boolean allOkInitialization = true;
+        String inputLineClient;
+
         do {
             System.out.println("How big the grid? 2:[2x2]  3:[3x3] 5:[5x5]");
             outputClient.write("How big the grid? 2:[2x2]  3:[3x3] 5:[5x5]" + System.lineSeparator());
             outputClient.flush();
 
+            inputLineClient = keyboard.readLine();
+
+            if (inputLineClient == null || inputLineClient.equals(quitCmd)) {
+                System.out.print(String.format("[%1$tY-%1$tm-%1$td %1$tT] ", System.currentTimeMillis()));
+                System.out.println("Client disconnected or Client abruptly closed connection");
+                return allOkInitialization = false;
+            }
+
             try {
-                optionGrid = Integer.parseInt(keyboard.readLine());
-            }catch (Exception e){
+                optionGrid = Integer.parseInt(inputLineClient);
+            } catch (Exception e) {
                 e.getStackTrace();
             }
         } while (!(optionGrid == 2 || optionGrid == 3 || optionGrid == 5));
@@ -56,14 +75,25 @@ public class ClientPlayerGame extends Game {
         board = new Board(optionGrid, optionGrid);
         graphic = new Graphic(optionGrid, optionGrid);
 
-        return board;
+        return allOkInitialization;
     }
 
-    public void startGame(){
+    public void startGame() {
         return;
     }
-    public Board initializeBoard(){
+
+    public Board initializeBoard() {
         Board board1 = new Board(1, 1);
         return board1;
+    }
+
+    public String printScoreBoardOnClient() {
+
+        String printableScoreBoard;
+        printableScoreBoard = graphic.getStringBoard() + System.lineSeparator();
+        printableScoreBoard += "Player " + player1.getId() + " got " + player1.getPoints() + " points" + System.lineSeparator();
+        printableScoreBoard += "Player " + player2.getId() + " got " + player2.getPoints() + " points" + System.lineSeparator();
+        printableScoreBoard += "Is the turn of Player" + currentPlayer.getId() + System.lineSeparator();
+        return printableScoreBoard;
     }
 }
