0ff3fe623080b1827be560f691be0195f3ddbbf4
JawaCoder <johndoe@example.com>
2021-01-04 12:20:45 +0100

    ServerGame: add welcome message, implemented the flow to allow client to end the game or restart a new one; Refactoring

--- src/main/java/ServerGame.java
+++ src/main/java/ServerGame.java
@@ -33,19 +33,20 @@ public class ServerGame {
                             System.out.print(String.format("[%1$tY-%1$tm-%1$td %1$tT] ", System.currentTimeMillis()));
                             System.out.println("New connection from: " + socket.getInetAddress().getHostName()); // Log the new connection client
 
-                            //TODO - welcome message on client screen
 
                             // istance new game
                             ClientPlayerGame gameForClient = new ClientPlayerGame(br, bw);
+
+                            bw.write("Welcome on Server 'Dot and Boxes'!" + System.lineSeparator());
+                            bw.write("Now you can play a game in your terminal!" + System.lineSeparator());
+                            bw.write("If you wanna exit the game type 'QUIT'" + System.lineSeparator());
+                            bw.flush();
                             gameForClient.startGameServer(quitCommand);
 
+                            while (true) {
+                                bw.write("you can restart a new game (type 'NEW') or actually quit (type 'QUIT')" + System.lineSeparator());
+                                bw.flush();
 
-                            while (true) {  //here the games is end or the client asked to quit
-
-                                //possible line showed to client: "the game is ended due to a quit request or to its natural end:
-                                // would you really quit (write QUIT) or restart a new game?"
-
-                                //TODO - implement a flow to allow a user to restart a new game
                                 String command = br.readLine();
 
                                 if (command == null) {
@@ -56,17 +57,32 @@ public class ServerGame {
 
                                 command = command.trim();
                                 if (command.equals(quitCommand)) {
+                                    bw.write("You chose to close the connection with the Server!" + System.lineSeparator());
+                                    bw.write("Thank you, we hope that enjoyed your game! See you");
+                                    bw.write("Type whatever you want to close the connection" + System.lineSeparator());
+                                    bw.flush();
+                                    try{
+                                        br.readLine();
+                                    }catch (Exception e){
+                                        e.printStackTrace();
+                                    }
                                     System.out.print(String.format("[%1$tY-%1$tm-%1$td %1$tT] ", System.currentTimeMillis()));
                                     System.out.println("Client " + socket.getInetAddress().getHostName() + " disconnected");
                                     break;
                                 }
+
+                                if (command.equals("NEW")) {
+                                    gameForClient = new ClientPlayerGame(br, bw);
+                                    gameForClient.startGameServer(quitCommand);
+                                }
+
                             }
                         } catch (IOException e) {
                             System.out.print(String.format("[%1$tY-%1$tm-%1$td %1$tT] ", System.currentTimeMillis()));
-                            System.out.printf("IO error: %s", e.getMessage());
+                            System.out.printf("IO error (Client " + socket.getInetAddress().getHostName() +"): %s", e.getMessage() + "\n");
                         } catch (Exception e) {
                             System.out.print(String.format("[%1$tY-%1$tm-%1$td %1$tT] ", System.currentTimeMillis()));
-                            System.out.printf("EXCEPTION: %s", e.getMessage());
+                            System.out.printf("EXCEPTION: %s", e.getMessage() + "\n");
                         }
                     });
                 } catch (IOException e) {
