8616ce8fde6ca4c55b6501da6d5494b7a3dde2c7
ivanzen97 <ivanzen@live.it>
2021-01-05 16:19:28 +0100

    implemented abstract class IOManager

--- src/main/java/IOManager.java
+++ src/main/java/IOManager.java
@@ -1,14 +1,6 @@
-public class IOManager {
-
-    /*
-    readMove () -> legge e manda la move a game
-
-    stampaMove --> modifica grafic
-    stampa puntiPlayer --> player 1 : 4 punti
-    stampa turno --> turno del player 1
-    stampa vittoria
-    coloraBox --> colora la box
-
-    */
-
+public abstract class IOManager {
+    public abstract Move readMove ();
+    public abstract void updateMove ();
+    public abstract void updateCompletedBox ();
+    public abstract void updateGameInfo ();
 }
