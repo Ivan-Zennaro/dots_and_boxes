0eb88ccb5d16dcd58f887eeb8a5540cb07acefa0
ivanzen97 <ivanzen@live.it>
2021-01-05 16:06:06 +0100

    Created class IOManager

---
+++ src/main/java/IOManager.java
@@ -0,0 +1,14 @@
+public class IOManager {
+
+    /*
+    readMove () -> legge e manda la move a game
+
+    stampaMove --> modifica grafic
+    stampa puntiPlayer --> player 1 : 4 punti
+    stampa turno --> turno del player 1
+    stampa vittoria
+    coloraBox --> colora la box
+
+    */
+
+}
