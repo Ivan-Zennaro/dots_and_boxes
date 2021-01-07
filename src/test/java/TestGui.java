713cfef52336dcb1edd982ee86c1032ef05926af
Dario Crosera <dario.cro@hotmail.it>
2021-01-07 17:39:36 +0100

    Add class TestGui

---
+++ src/test/java/TestGui.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+
+public class TestGui {
+
+  /*  @Test
+    public void one_move_inserted(){
+        Player p1 = new Player('A', Color.RED);
+        Player p2 = new Player('B', Color.BLU);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Gui g = new Gui(3,3, frame, p1, p2);
+        TwoPlayersNewGame t = new TwoPlayersNewGame(3,3, p1, p2, g);
+        t.startGame();
+    }
+*/
+
+
+
+
+}
