770388777c5e4384b979ede37ba77bba137e2097
Dario Crosera <dario.cro@hotmail.it>
2021-01-08 15:05:24 +0100

    Test Gui a_line_isnt_colored_at_init passed

--- src/test/java/TestGui.java
+++ src/test/java/TestGui.java
@@ -1,10 +1,24 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
 
 
 public class TestGui {
 
-  /*  @Test
-    public void one_move_inserted(){
+    @Test
+    public void a_line_isnt_colored_at_init() {
+        Player p1 = new Player('A', Color.RED);
+        Player p2 = new Player('B', Color.BLU);
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Gui g = new Gui(3, 3, frame, p1, p2);
+        Move testMove = new Move(1, 1, Side.UP);
+        Assertions.assertFalse(g.isSetEdge(IOManager.getMappedX(testMove), IOManager.getMappedY(testMove)));
+    }
+
+    @Test
+    public void line_colored_when_move_inserted() {
         Player p1 = new Player('A', Color.RED);
         Player p2 = new Player('B', Color.BLU);
         JFrame frame = new JFrame();
