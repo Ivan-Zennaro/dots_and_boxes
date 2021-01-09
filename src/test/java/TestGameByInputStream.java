9a7b6523c856c024a6ed916ca4d7e79bf9d79267
Domenis Nicola S262688 <nicoladomenis23@hotmail.it>
2021-01-09 10:40:56 +0100

    Now one test from TestGameByInputStream works for TwoPlayersNewGame

--- src/test/java/TestGameByInputStream.java
+++ src/test/java/TestGameByInputStream.java
@@ -1,13 +1,23 @@
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class TestGameByInputStream {
+
+    private final int boardSize = 2;
+    private final Player player1 = new Player('A', Color.BLU);
+    private final Player player2 = new Player('B', Color.RED);
+
+
+    /*
     @ParameterizedTest
     @ValueSource(strings = {"2","3","5"}) //Value Source quando ho solo un parametro
     public void check_input_grid_dimensions(String choice){
@@ -20,29 +30,33 @@ public class TestGameByInputStream {
                 () -> Assertions.assertEquals(Integer.parseInt(choice), board.getBoardRows()),
                 () -> Assertions.assertEquals(Integer.parseInt(choice), board.getBoardColumns())
         );
-    }
+    }*/
 
     @Test
-    public void game_run_from_input_stream(){
-        String inputString = "2"+System.lineSeparator()+
-                                "0 0 L"+System.lineSeparator()+
-                             "0 0 R"+System.lineSeparator()+
-                "0 0 U"+System.lineSeparator()+
-                "0 0 D"+System.lineSeparator()+
-                "0 1 U"+System.lineSeparator()+
-                "0 1 D"+System.lineSeparator()+
-                "0 1 R"+System.lineSeparator()+
-                "1 1 L" +System.lineSeparator()+
-                "1 1 R" +System.lineSeparator()+
-                "1 1 D" +System.lineSeparator()+
-                "1 0 D" +System.lineSeparator()+
-                "1 0 L"+System.lineSeparator();
-        ByteArrayInputStream in = new ByteArrayInputStream(inputString.getBytes());
-        System.setIn(in);
-        TwoPlayersGame game = new TwoPlayersGame();
-        game.startGame();
-        game.endGame();
-        Assertions.assertTrue(game.isGameFinished());
-    }
+    public void game_run_from_input_stream() {
+        String inputString = """
+                0 0 L
+                0 0 R
+                0 0 U
+                0 0 D
+                0 1 U
+                0 1 D
+                0 1 R
+                1 1 L
+                1 1 R
+                1 1 D
+                1 0 D
+                1 0 L""";
+        System.setIn(new ByteArrayInputStream(inputString.getBytes()));
+        Cli cli = new Cli(boardSize, boardSize);
+        TwoPlayersNewGame game = new TwoPlayersNewGame(2, 2, player1, player2, cli);
 
+        //cli.initialize();
+        game.startGame();
+        //game.endGame();
+        Assertions.assertTrue(game.isGameFinished());
+
+    }
 }
+
+
