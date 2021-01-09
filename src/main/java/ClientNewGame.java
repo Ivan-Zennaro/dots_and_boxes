19b09c738b46c34ed505693d6e87cebf379161e2
JawaCoder <johndoe@example.com>
2021-01-09 19:42:02 +0100

    [ClientNewGame] add IOException, add comments, refactor names in constructor

--- src/main/java/ClientNewGame.java
+++ src/main/java/ClientNewGame.java
@@ -2,20 +2,18 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-
 public class ClientNewGame extends NewGame {
 
     private final int PORT = 1234;
     private String ip;
 
-    public ClientNewGame(int nRows, int nCols, Player p1, Player p2, IOManager ioManager, String ip) {
-        super(nRows, nCols, p1, p2, ioManager);
+    public ClientNewGame(int nRows, int nCols, Player player1, Player player2, IOManager ioManager, String ip) {
+        super(nRows, nCols, player1, player2, ioManager);
         this.ip = ip;
     }
 
     @Override
     public void startGame() {
-
         try (Socket socket = new Socket(ip, PORT)) {
 
             OutputStream outputStream = socket.getOutputStream();
@@ -24,33 +22,34 @@ public class ClientNewGame extends NewGame {
             InputStream inputStream = socket.getInputStream();
             ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
 
-
             printScoreBoard();
+
             while (!isGameFinished()) {
-
-
                 Move move;
 
                 if (currentPlayer == player2) {
                     move = ioManager.readMove();
-                    if (isMoveAllowed(move)) {
+
+                    if (isMoveAllowed(move)) { //...send the move to the Server
                         objectOutputStream.writeObject(move);
                         objectOutputStream.flush();
                     }
-
                 } else {
-
                     move = (Move) objectInputStream.readObject();
                 }
+
                 computeMove(move);
                 printScoreBoard();
             }
 
             endGame();
 
+        } catch (IOException e) {
+            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
+
         //TODO torno al menu iniziale
     }
 }
