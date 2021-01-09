6e6141bb22b70a1b3a92130d9ba268e8ed91c4dd
JawaCoder <johndoe@example.com>
2021-01-09 19:34:25 +0100

    [MainServer] Refactor with a rename of object Player; removed comments

--- src/main/java/MainServer.java
+++ src/main/java/MainServer.java
@@ -2,23 +2,9 @@ import java.io.IOException;
 
 public class MainServer {
     public static void main(String[] args) {
-
-
-        /*ServerGame sg = new ServerGame(10000, "QUIT");
-        try {
-            sg.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        */
-
-        Player p1 = new Player('A',Color.RED);
-        Player p2 = new Player('B',Color.BLU);
-        ServerNewGame game = new ServerNewGame(3,3,p1 ,p2,new Cli(3,3));
+        Player player1 = new Player('A', Color.RED);
+        Player player2 = new Player('B', Color.BLU);
+        ServerNewGame game = new ServerNewGame(3, 3, player1, player2, new Cli(3, 3));
         game.startGame();
-
     }
-
-
-
 }
