1f54a2377afdf91e4b27a45befd56d8c9393d1b4
JawaCoder <johndoe@example.com>
2021-01-09 19:34:46 +0100

    [MainClient] Refactor with a rename of object Player

--- src/main/java/MainClient.java
+++ src/main/java/MainClient.java
@@ -1,12 +1,10 @@
 public class MainClient {
 
     public static void main(String[] args) {
+        Player player1 = new Player('A', Color.RED);
+        Player player2 = new Player('B', Color.BLU);
 
-        Player p1 = new Player('A',Color.RED);
-        Player p2 = new Player('B',Color.BLU);
-        ClientNewGame game = new ClientNewGame(3,3,p1 ,p2,new Cli(3,3),"127.0.0.1");
+        ClientNewGame game = new ClientNewGame(3, 3, player1, player2, new Cli(3, 3), "127.0.0.1");
         game.startGame();
-
-
     }
 }
