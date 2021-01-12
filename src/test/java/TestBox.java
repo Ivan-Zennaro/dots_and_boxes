65c1c8323b3f39b2dc3ead2a8680660b857e3920
ivanzen97 <ivanzen@live.it>
2021-01-12 22:18:58 +0100

    Renamed test in correct_numeber_of_draw_line_in_box

--- src/test/java/TestBox.java
+++ src/test/java/TestBox.java
@@ -1,21 +1,11 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
-
 import java.util.stream.Stream;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestBox {
-
-    /*
-    Does this test make some sense? to test the instance of an object....dont think so.
-    [Dario]
-     */
-    //test1
 
     @Test
     public void empty_instance() {
@@ -41,18 +31,14 @@ public class TestBox {
         );
     }
 
-
-
-
-
     private static Stream<Arguments> boxAndMoveGenerator() {
-
         return Stream.of(
                 Arguments.of(new Box(true, true, false, false), new Move(1, 2, Side.UP)),
                 Arguments.of(new Box(false, true, true, false), new Move(1, 2, Side.DOWN)),
                 Arguments.of(new Box(false, true, false, false), new Move(1, 2, Side.LEFT))
-                );
+        );
     }
+
     @ParameterizedTest
     @MethodSource("boxAndMoveGenerator")
     public void checkLineInBoxByMove(Box box,Move move) {
@@ -60,7 +46,7 @@ public class TestBox {
     }
 
     @Test
-    public void correct_numeber_of_draw_in_box(){
+    public void correct_numeber_of_draw_line_in_box(){
         assertAll(
                 () -> assertEquals(4,new Box(true,true,true,true).getNumberOfDrawLine()),
                 () -> assertEquals(3,new Box(false,true,true,true).getNumberOfDrawLine()),
@@ -68,6 +54,5 @@ public class TestBox {
                 () -> assertEquals(1,new Box(true,false,false,false).getNumberOfDrawLine())
         );
     }
-
 
 }
