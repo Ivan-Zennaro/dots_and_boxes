05a9714e601f8f761dd41443e6c7d361fe8bf9c5
Domenis Nicola S262688 <nicoladomenis23@hotmail.it>
2021-01-12 09:48:15 +0100

    Corrected RulesPage Class

--- src/main/java/RulesPage.java
+++ src/main/java/RulesPage.java
@@ -4,14 +4,18 @@ public class RulesPage{
     private JFrame  frame ;
     private JTextArea textArea;
     private JPanel panel;
-    private String rulesText = "Welcome to Dots and Boxes! \n" +
-            "The rules are:\n"+
-            "1. Choose the players'identity and colour and the grid size;\n"+
-            "2. At each Player's turn, a line from the game grid must be selected;\n"+
-            "3. The player who completes a box gets a point and has an extra turn;\n "+
-            "4. Two players compete to complete most boxes in the game grid \n and to get more points!\n \n" +
-            "5.You can also choose to host or join a server-based game on the main menu\n"+
-            "Enjoy the game!";
+    private String rulesText = """
+            Welcome to Dots and Boxes!\s
+            
+            The rules are:
+            1. Choose the players' identity and colour and the grid size;
+            2. At each Player's turn, a line from the game grid must be selected;
+            3. The player who completes a box gets a point and has an extra turn;
+             4. Two players compete to complete most boxes in the game grid\s
+             and to get more points!
+            5.You can also choose to host or join a server-based game on the main menu.
+            
+            Enjoy the game!""";
 
 
     public RulesPage(){
@@ -38,6 +42,7 @@ public class RulesPage{
 
     public static void main(String[] args) {
         RulesPage rulesPage = new RulesPage();
+        rulesPage.seeFrame();
     }
 
 }
