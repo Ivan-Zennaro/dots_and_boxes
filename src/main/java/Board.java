0878e057c161a513b964efa031165cef57c7033b
ivanzen97 <ivanzen@live.it>
2021-01-12 21:41:48 +0100

    Indent and delete some spaces from Board class

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -4,9 +4,8 @@ public class Board {
     private int boardRows;
     private int boardColumns;
 
-
     public Board(int numberOfBoxesInARow, int numberOfBoxesInAColumn) {
-        board = new Box[numberOfBoxesInARow][numberOfBoxesInAColumn];
+        this.board = new Box[numberOfBoxesInARow][numberOfBoxesInAColumn];
         this.boardRows = numberOfBoxesInARow;
         this.boardColumns = numberOfBoxesInAColumn;
         for (int i = 0; i < numberOfBoxesInARow; i++) {
@@ -16,13 +15,12 @@ public class Board {
         }
     }
 
-    public boolean boxHasAlreadyLine(Move move) {       //AGGIUNGERE TEST!
+    public boolean boxHasAlreadyLine(Move move) {
         Box choosenBox = board[move.getX()][move.getY()];
         return choosenBox.hasLineBySide(move.getSide());
     }
 
     public void drawLine(Move move) {
-
         Box choosenBox = board[move.getX()][move.getY()];
         choosenBox.drawLine(move.getSide());
 
@@ -33,10 +31,34 @@ public class Board {
         }
     }
 
-
-
     public boolean isMoveInBoardRange(Move move) {
         return move.getX() < boardRows && move.getY() < boardColumns && move.getX() >= 0 && move.getY() >= 0;
+    }
+
+    public Move getNeighbourSideMove(Move move) {
+        if (move.getSide() == Side.UP || move.getSide() == Side.DOWN)
+            if (isMoveInBoardRange(new Move(move.getX() + move.getCoordShift(), move.getY(), move.getInvertedSide()))) {
+                return new Move(move.getX() + move.getCoordShift(), move.getY(), move.getInvertedSide());
+
+            }
+        if (move.getSide() == Side.LEFT || move.getSide() == Side.RIGHT)
+            if (isMoveInBoardRange(new Move(move.getX(), move.getY() + move.getCoordShift(), move.getInvertedSide()))) {
+                return new Move(move.getX(), move.getY() + move.getCoordShift(), move.getInvertedSide());
+            }
+
+        return new Move(-1, -1, Side.INVALID);
+    }
+
+    public Box getBoxByMove(Move move) {
+        return board[move.getX()][move.getY()];
+    }
+
+    public boolean isBoxCompleted(Move move) {
+        return getBoxByMove(move).isCompleted();
+    }
+
+    public Box[][] getBoard() {
+        return board;
     }
 
     public int getBoardRows() {
@@ -46,28 +68,4 @@ public class Board {
     public int getBoardColumns() {
         return boardColumns;
     }
-
-    public Move getNeighbourSideMove(Move move){
-        if (move.getSide() == Side.UP || move.getSide() == Side.DOWN)
-            if (isMoveInBoardRange(new Move(move.getX() + move.getCoordShift(), move.getY(), move.getInvertedSide()))) {
-                return new Move (move.getX()+ move.getCoordShift(),move.getY() ,move.getInvertedSide());
-
-            }
-        if (move.getSide() == Side.LEFT || move.getSide() == Side.RIGHT)
-            if (isMoveInBoardRange(new Move(move.getX(), move.getY() + move.getCoordShift(), move.getInvertedSide()))) {
-                return new Move (move.getX(),move.getY() + move.getCoordShift(),move.getInvertedSide());
-            }
-
-        return new Move(-1,-1,Side.INVALID);
-    }
-
-    public Box getBoxByMove(Move move) {
-        return board[move.getX()][move.getY()];
-    }
-
-    public boolean isBoxCompleted(Move move){
-        return getBoxByMove(move).isCompleted();
-    }
-
-    public Box[][] getBoard() {return board; }
 }
