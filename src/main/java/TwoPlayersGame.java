408a279bc4bbe2ffe9fe5423cce6bb02a2c733bc
ivanzen97 <ivanzen@live.it>
2021-01-12 20:12:18 +0100

    delete unused constructor

--- src/main/java/TwoPlayersGame.java
+++ src/main/java/TwoPlayersGame.java
@@ -1,13 +1,7 @@
-import java.lang.reflect.InvocationTargetException;
-
 public class TwoPlayersGame extends Game {
 
     public TwoPlayersGame(int nRows, int nCols, Player p1, Player p2, IOManager ioManager) {
         super(nRows, nCols, p1, p2, ioManager);
-    }
-
-    public TwoPlayersGame(int nRows, int nCols, Player p1, Player p2, Class c) throws IllegalAccessException, InvocationTargetException, InstantiationException {
-        super(nRows, nCols, p1, p2, (IOManager) c.getDeclaredConstructors()[0].newInstance(nRows, nCols, p1, p2));
     }
 
     @Override
