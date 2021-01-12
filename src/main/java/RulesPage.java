580bb58308e749789e794b24844b7aa0522a4f93
Domenis Nicola S262688 <nicoladomenis23@hotmail.it>
2021-01-12 09:52:52 +0100

    Refactored RulesPage Class

--- src/main/java/RulesPage.java
+++ src/main/java/RulesPage.java
@@ -1,24 +1,24 @@
 import javax.swing.*;
 
-public class RulesPage{
-    private JFrame  frame ;
+public class RulesPage {
+    private JFrame frame;
     private JTextArea textArea;
     private JPanel panel;
     private String rulesText = """
             Welcome to Dots and Boxes!\s
-            
+                        
             The rules are:
             1. Choose the players' identity and colour and the grid size;
             2. At each Player's turn, a line from the game grid must be selected;
             3. The player who completes a box gets a point and has an extra turn;
-             4. Two players compete to complete most boxes in the game grid\s
-             and to get more points!
+            4. Two players compete to complete most boxes in the game grid\s
+            and to get more points!
             5.You can also choose to host or join a server-based game on the main menu.
-            
+                        
             Enjoy the game!""";
 
 
-    public RulesPage(){
+    public RulesPage() {
         this.frame = new JFrame("Dots and Boxes rules");
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 
@@ -32,17 +32,9 @@ public class RulesPage{
         frame.add(panel);
         frame.pack();
         frame.setResizable(false);
-
-
-
     }
-    public void seeFrame(){
+
+    public void seeFrame() {
         frame.setVisible(true);
     }
-
-    public static void main(String[] args) {
-        RulesPage rulesPage = new RulesPage();
-        rulesPage.seeFrame();
-    }
-
 }
