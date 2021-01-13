678fc4297ad744a194dac69e3d702c999662863e
ivanzen97 <ivanzen@live.it>
2021-01-13 17:03:16 +0100

    new test name_start_with_the_correct_letter passed

--- src/test/java/TestPlayer.java
+++ src/test/java/TestPlayer.java
@@ -2,20 +2,21 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 public class TestPlayer {
 
     @Test
     public void update_point_from_0_to_1(){
         Player player = new Player("A", Color.RED);
-        Assertions.assertEquals(1, player.onePointDone());
+        player.onePointDone();
+        Assertions.assertEquals(1, player.getPoints());
     }
 
     @ParameterizedTest
-    @CsvSource({"RED", "BLU", "PURPLE", "GREEN"})
-    public void color_is_mapped_to_the_correct_awtColor(Color color){
-        Player player = new Player("A", color);
-        Assertions.assertEquals(color.getAwtColor(), player.getColor().getAwtColor());
+    @CsvSource({"John,J", "Luke,L", "Mike,M", "Carl,C"})
+    public void name_start_with_the_correct_letter(String name, char firstLetter){
+        Player player = new Player(name, Color.RED);
+        Assertions.assertEquals(firstLetter, player.getFirstLetterName());
     }
+
 }
