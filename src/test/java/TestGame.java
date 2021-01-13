86b39cb5d93a36a6650f651ee3ebc02e6783ec12
Dario Crosera <dario.cro@hotmail.it>
2021-01-13 18:53:24 +0100

    Refactored TestGame

--- src/test/java/TestGame.java
+++ src/test/java/TestGame.java
@@ -1,91 +1,83 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class TestGame {
     private final int boardSize = 2;
-    private final Player player1 = new Player("A",Color.BLU);
-    private final Player player2 = new Player("B",Color.RED);
-    private final Cli cli = new Cli(boardSize,boardSize,player1, player2);
-    private final GameStub game = new GameStub(2,2,player1,player2,cli);
+    private final Player player1 = new Player("A", Color.BLU);
+    private final Player player2 = new Player("B", Color.RED);
+    private final Cli cli = new Cli(boardSize, boardSize, player1, player2);
+    private final GameStub game = new GameStub(2, 2, player1, player2, cli);
 
     @Test
-    public void game_is_not_end(){
-        System.out.println("GAME HAS NOT ENDED TEST");
+    public void is_not_ended() {
+        game.computeMove(new Move(0, 0, Side.UP));
 
-        game.computeMoveByString("0 0 U");
         Assertions.assertFalse(game.isGameFinished());
     }
 
     @Test
-    public void game_ended(){
-        System.out.println("GAME ENDED TEST");
+    public void is_ended() {
+        game.computeMove(new Move(0, 0, Side.LEFT));
+        game.computeMove(new Move(0, 0, Side.RIGHT));
+        game.computeMove(new Move(0, 0, Side.UP));
+        game.computeMove(new Move(0, 0, Side.DOWN));
 
 
+        game.computeMove(new Move(0, 0, Side.LEFT));
+        game.computeMove(new Move(0, 0, Side.RIGHT));
+        game.computeMove(new Move(0, 0, Side.UP));
+        game.computeMove(new Move(0, 0, Side.DOWN));
 
-        game.computeMoveByString("0 0 L");
+        game.computeMove(new Move(0, 1, Side.UP));
+        game.computeMove(new Move(0, 1, Side.DOWN));
+        game.computeMove(new Move(0, 1, Side.RIGHT));
 
-        game.computeMoveByString("0 0 R");
-        game.computeMoveByString("0 0 U");
-        game.computeMoveByString("0 0 D");
+        game.computeMove(new Move(1, 1, Side.LEFT));
+        game.computeMove(new Move(1, 1, Side.RIGHT));
+        game.computeMove(new Move(1, 1, Side.DOWN));
 
-        game.computeMoveByString("0 0 R");
-        game.computeMoveByString("0 0 U");
-        game.computeMoveByString("0 0 D");
-        game.computeMoveByString("0 1 U");
-        game.computeMoveByString("0 1 D");
-        game.computeMoveByString("0 1 R");
-        game.computeMoveByString("1 1 L");
-        game.computeMoveByString("1 1 R");
-        game.computeMoveByString("1 1 D");
-        game.computeMoveByString("1 0 D");
-        game.computeMoveByString("1 0 L");
+        game.computeMove(new Move(1, 0, Side.DOWN));
+        game.computeMove(new Move(1, 0, Side.LEFT));
         game.endGame();
 
         Assertions.assertTrue(game.isGameFinished());
     }
 
     @Test
-    public void game_one_points(){
-        System.out.println("TEST GAME ONE POINT");
+    public void one_point_to_player2() {
+        game.computeMove(new Move(0, 0, Side.LEFT));
+        game.computeMove(new Move(0, 0, Side.DOWN));
+        game.computeMove(new Move(0, 0, Side.UP));
+        game.computeMove(new Move(0, 1, Side.LEFT));
 
-
-        game.computeMoveByString("0 0 L");
-        game.computeMoveByString("0 0 U");
-        game.computeMoveByString("0 0 D");
-        game.computeMoveByString("0 1 L");
-
-
-        Assertions.assertEquals(1,game.printCurrentPlayerScore());
+        Assertions.assertEquals(1, game.player2.getPoints());
 
     }
 
     @Test
-    public void game_first_turn_player(){
-        System.out.println("TEST GAME FIRST TURN");
-        Assertions.assertEquals(game.player1.getFirstLetterName(),game.currentPlayer.getFirstLetterName());
+    public void player1_plays_first() {
+        Assertions.assertEquals(game.player1, game.currentPlayer);
     }
 
     @Test
-    public void game_second_turn_player(){
-        System.out.println("TEST GAME SECOND TURN");
+    public void player2_plays_second() {
+        game.computeMove(new Move(0, 0, Side.LEFT));
 
-        game.computeMoveByString("0 0 L");
-        Assertions.assertEquals(game.player2.getFirstLetterName(),game.currentPlayer.getFirstLetterName());
+        Assertions.assertEquals(game.player2, game.currentPlayer);
     }
 
     @Test
-    public void  game_swap_players_after_one_point(){
-        System.out.println("TEST GAME TURN AFTER POINT");
-
-        game.computeMoveByString("0 0 L");
-        game.computeMoveByString("0 0 U");
-        game.computeMoveByString("0 0 D");
-        game.computeMoveByString("0 1 L");
+    public void game_swap_players_after_one_point() {
+        game.computeMove(new Move(0, 0, Side.LEFT));
+        game.computeMove(new Move(0, 0, Side.DOWN));
+        game.computeMove(new Move(0, 0, Side.UP));
+        game.computeMove(new Move(0, 1, Side.LEFT));
 
         assertAll(
-                ()->Assertions.assertEquals(game.player2.getFirstLetterName(),game.currentPlayer.getFirstLetterName()),
-                ()->Assertions.assertNotEquals(game.player1.getFirstLetterName(),game.currentPlayer.getFirstLetterName())
+                () -> Assertions.assertEquals(game.player2, game.currentPlayer),
+                () -> Assertions.assertNotEquals(game.player1, game.currentPlayer)
         );
 
     }
