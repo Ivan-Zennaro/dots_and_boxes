969479be69af7e70db732509c47253a149f200ce
Dario Crosera <dario.cro@hotmail.it>
2021-01-13 17:35:17 +0100

    refactored class TestMoveParsing

--- src/test/java/TestMoveParsing.java
+++ src/test/java/TestMoveParsing.java
@@ -1,26 +1,26 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Scanner;
 
-public class TestInputMoves {
+public class TestMoveParsing {
 
-
-   //private final Input input = new Input();
-
-    @Test
-    public void testMoveParser(){
-        Move inputMove = Move.parseMove("10 5 U");
-        Move testMove = new Move(10,5,Side.UP);
+    @ParameterizedTest
+    @CsvSource({"10,5,UP,10 5 U", "0,0,DOWN,0 0 D", "1,0,RIGHT,1 0 R", "1,1,LEFT, 1 1 L"})
+    public void string_to_move(int x, int y, Side side, String stringMove){
+        Move inputMove = Move.parseMove(stringMove);
+        Move testMove = new Move(x,y,side);
 
         Assertions.assertEquals(testMove,inputMove);
     }
+
     @Test
-    public void testMoveNull(){
+    public void null_to_invalid_move(){
         Move inputMove = Move.parseMove(null);
         Move testMove = new Move(-1,-1,Side.INVALID);
 
@@ -28,13 +28,14 @@ public class TestInputMoves {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"4 5 k", "u p k", "487 K"}) //L'input è per le stringhe di lunghezza 3, la convenzione si può modificare come preferiamo, basta decidere
-    public void testInvalidMoveFromInput(String inpt){
-        Move inputMove = Move.parseMove(inpt);
+    @ValueSource(strings = {"4 5 k", "u p k", "487 K", "01D", "0", "", "0 0 D"}) //L'input è per le stringhe di lunghezza 3, la convenzione si può modificare come preferiamo, basta decidere
+    public void string_to_invalid_move(String input){
+        Move inputMove = Move.parseMove(input);
         Move testMove = new Move(-1,-1,Side.INVALID);
 
         Assertions.assertEquals(testMove,inputMove);
     }
+
     @Test
     public void testPlayerInput() {
         String data = "4 5 U";
