09e3cfdff933cab913ba8f4aa429e30d51de9cca
ivanzen97 <ivanzen@live.it>
2021-01-16 15:23:56 +0100

    Refactored color

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -11,19 +11,17 @@ public enum Color {
                     case RED -> "\u001B[31m";
                     case PURPLE -> "\u001B[35m";
                     case GREEN -> "\u001B[32m";
-                    default -> "\u001B[0m";
                 }
         ) + string + "\u001B[0m";
     }
 
     public java.awt.Color getAwtColor() {
-        switch (this) {
-            case BLU : return new java.awt.Color(59, 105, 177);
-            case RED : return new java.awt.Color(244, 67, 54);
-            case PURPLE : return new java.awt.Color(135, 23, 191);
-            case GREEN : return new java.awt.Color(117, 181, 43);
-        }
-        return null;
+        return switch (this) {
+            case BLU -> new java.awt.Color(59, 105, 177);
+            case RED -> new java.awt.Color(244, 67, 54);
+            case PURPLE -> new java.awt.Color(135, 23, 191);
+            case GREEN -> new java.awt.Color(117, 181, 43);
+        };
     }
 
     public String getRGBstring() {
