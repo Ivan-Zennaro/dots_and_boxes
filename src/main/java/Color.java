a176d96f22d8f9e4af6be4254fabe52811e614d0
ivanzen97 <ivanzen@live.it>
2021-01-17 19:09:13 +0100

    Improved efficiency on Color class

--- src/main/java/Color.java
+++ src/main/java/Color.java
@@ -1,31 +1,27 @@
 public enum Color {
-    RED,
-    BLU,
-    GREEN,
-    PURPLE;
+
+    RED ("\u001B[31m",new java.awt.Color(244, 67, 54)),
+    BLU ("\u001B[34m",new java.awt.Color(59, 105, 177)),
+    GREEN("\u001B[32m",new java.awt.Color(117, 181, 43)),
+    PURPLE("\u001B[35m",new java.awt.Color(135, 23, 191));
+
+    private String cliColorCode;
+    private java.awt.Color awtColor;
+
+    Color (String cliColorCode, java.awt.Color awtColor){
+        this.cliColorCode = cliColorCode;
+        this.awtColor = awtColor;
+    }
 
     public String getColoredString(String string) {
-        return (
-                switch (this) {
-                    case BLU -> "\u001B[34m";
-                    case RED -> "\u001B[31m";
-                    case PURPLE -> "\u001B[35m";
-                    case GREEN -> "\u001B[32m";
-                }
-        ) + string + "\u001B[0m";
+        return cliColorCode + string + "\u001B[0m";
     }
 
     public java.awt.Color getAwtColor() {
-        return switch (this) {
-            case BLU -> new java.awt.Color(59, 105, 177);
-            case RED -> new java.awt.Color(244, 67, 54);
-            case PURPLE -> new java.awt.Color(135, 23, 191);
-            case GREEN -> new java.awt.Color(117, 181, 43);
-        };
+        return awtColor;
     }
 
     public String getRGBstring() {
-        java.awt.Color awtColor = this.getAwtColor();
         return "rgb(" + awtColor.getRed() + "," + awtColor.getGreen() + "," + awtColor.getBlue() + ")";
     }
 }
